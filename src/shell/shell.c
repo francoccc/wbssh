454d1251ea8ee71f09e171fe4008aa63373fc940
francoccc <frcc.fncme@gmail.com>
2024-02-26 14:03:34 +0800

    fix typo

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -36,18 +36,15 @@ char** split_line(char* line) {
 	token = strtok(line, TOK_DELIM);
 	while (token != NULL) {
 		/* handle comments */
-		if (token[0] == '#')
-		{
+		if (token[0] == '#') {
 			break;
 		}
 		tokens[i] = token;
 		i++;
-		if (i >= bufsize)
-		{
+		if (i >= bufsize) {
 			bufsize += bufsize;
 			tokens = realloc(tokens, bufsize * sizeof(char*));
-			if (!tokens)
-			{
+			if (!tokens) {
 				fprintf(stderr, "reallocation error in split_line: tokens");
 				exit(EXIT_FAILURE);
 			}
